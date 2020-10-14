34aac592c9f56fb8623a55167cd2366fe7fe3450
Artem Tolstykh <atolstykh@mmdsmart.com>
2020-10-14 17:04:10 +0300

    wip

---
+++ src/layouts/primitives/Grid.svelte
@@ -0,0 +1,23 @@
+<script>
+  export let min = '250px'
+  export let space = 'var(--s1)'
+</script>
+
+<div
+  class="grid"
+  style="--min: {min}; --space: {space === '0' ? '0px' : space}">
+  <slot />
+</div>
+
+<style>
+  .grid {
+    display: grid;
+    grid-gap: var(--space);
+  }
+  
+  @supports (width: min(var(--min), 100%)) {
+    .grid {
+      grid-template-columns: repeat(auto-fit, minmax(min(var(--min), 100%), 1fr));
+    }
+  }
+</style>

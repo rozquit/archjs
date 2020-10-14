eb0579cbcceb00b72811d9956aaee3ae2d1bee66
Artem Tolstykh <atolstykh@mmdsmart.com>
2020-10-14 15:27:32 +0300

    wip

---
+++ src/layouts/primitives/Cluster.svelte
@@ -0,0 +1,29 @@
+<script>
+  export let justify = 'center'
+  export let align = 'center'
+  export let space = 'var(--s1)'
+</script>
+
+<div
+  class="cluster"
+  style="--justify: {justify}; --align: {align}; --space: {space === '0' ? '0px' : space}">
+  <slot />
+</div>
+
+<style>
+  .cluster {
+    overflow: hidden;
+  }
+  
+  .cluster > :global(*) {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: var(--justify);
+    align-items: var(--align);
+    margin: calc(var(--space) / 2 * -1);
+  }
+  
+  .cluster > :global(*) > :global(*) {
+    margin: calc(var(--space) / 2);
+  }
+</style>

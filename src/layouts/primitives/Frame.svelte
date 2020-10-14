fe5d12b937d9d28ffc7977a306f07ff29e80c7a4
Artem Tolstykh <atolstykh@mmdsmart.com>
2020-10-14 17:48:03 +0300

    wip

---
+++ src/layouts/primitives/Frame.svelte
@@ -0,0 +1,34 @@
+<script>
+  export let ratio = '16:9'
+  const [d, n] = ratio.split(':')
+</script>
+
+<div class="frame" style="--n: {n}; --d: {d}">
+  <slot />
+</div>
+
+<style>
+  .frame {
+    padding-bottom: calc(var(--n) / var(--d) * 100%);
+    position: relative;
+  }
+  
+  .frame > :global(*) {
+    overflow: hidden;
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  
+  .frame > :global(img),
+  .frame > :global(video) {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+</style>

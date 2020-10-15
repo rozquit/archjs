4e8b008f264c627ccd782146bbfa2950301c6028
Artem Tolstykh <atolstykh@mmdsmart.com>
2020-10-15 19:14:40 +0300

    wip

---
+++ src/layouts/primitives/Imposter.svelte
@@ -0,0 +1,27 @@
+<script>
+  export let breakout = false
+  export let margin = 0
+  export let fixed = false
+</script>
+
+<div
+  class="imposter"
+  class:contain={breakout}
+  style="--margin: {margin}; --position: {fixed ? 'fixed' : 'absolute'}">
+  <slot />
+</div>
+
+<style>
+  .imposter {
+    position: var(--position);
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+  
+  .imposter.contain {
+    overflow: auto;
+    max-width: calc(100% - (var(--margin) * 2));
+    max-height: calc(100% - (var(--margin) * 2));
+  }
+</style>
